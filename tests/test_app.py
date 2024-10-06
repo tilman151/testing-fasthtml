6438f1553dc372ffdfe6132318ec0864e7cfa127
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-10-06 22:01:36 +0200

    test: add e2e tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ import re
 
 import pytest
 import htmlmin
+from playwright.sync_api import Page
 
 from app import database
 
@@ -74,5 +75,42 @@ def test_history_swaps_button(client):
 
 
 @pytest.mark.e2e
-def test_e2e_dummy(server):
-    assert True
+def test_ask_question(server, page: Page):
+    """When I ask a question, it should show the question and answer."""
+    page.goto("http://localhost:5001")
+    page.get_by_label("Question").type("How are you doing?")
+    page.get_by_text("Ask").click()
+    page.wait_for_selector("input[name='answer']")
+
+    assert page.get_by_label("Question").input_value() == "How are you doing?"
+    assert page.get_by_label("Answer").input_value()
+    assert page.get_by_text("Reset").is_visible()
+
+
+@pytest.mark.e2e
+def test_ask_question_history(server, page: Page):
+    """When I ask a question,
+    I should be able to ask again from the history by clicking it."""
+    page.goto("http://localhost:5001")
+    page.get_by_label("Question").type("How are you doing?")
+    page.get_by_text("Ask").click()
+    page.wait_for_selector("input[name='answer']")
+    page.get_by_text("Reset").click()
+    page.wait_for_selector("#history-button")
+
+    page.locator("css=#history-button").click()
+    page.wait_for_selector("table")
+    assert page.locator("css=table tbody tr").count() == 1
+    page.locator("css=table tbody tr").click()
+    page.wait_for_selector("input[name='answer']")
+
+
+@pytest.mark.e2e
+def test_toggle_history(server, page: Page):
+    """When I click the history button, I should be able to toggle the history."""
+    page.goto("http://localhost:5001")
+    page.locator("css=#history-button").click()
+    page.wait_for_selector("table")
+    page.locator("css=#history-button").click()
+
+    assert page.locator("css=table").count() == 0
