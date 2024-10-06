744f1ea8304d4c9e6da4ff67446688d8f857bc9c
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-10-06 17:02:06 +0200

    feat: read database URL from env var

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
+import os
+
 import ollama
 from fasthtml import ft, pico
 from fasthtml.common import fast_app, serve
 
 from app import utils, database
 
+
 bootstrap_icons = (
     "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
 )
 bootstrap_icons_link = ft.Link(rel="stylesheet", href=bootstrap_icons)
+database_url = os.environ.get("APP_DATABASE_URL", "sqlite:///history.db")
 app, rt = fast_app(
     hdrs=(bootstrap_icons_link,),
-    on_startup=[utils.pull_llm, database.connect, database.init],
+    on_startup=[utils.pull_llm, lambda: database.connect(database_url), database.init],
     on_shutdown=[database.disconnect],
 )
 
