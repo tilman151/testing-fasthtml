9ff8d5cfcb531e54775eb70f48d2c10962b05375
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-10-06 12:17:10 +0200

    feat: add interface to ask a question

--- app/main.py
+++ app/main.py
@@ -1,11 +1,38 @@
+import ollama
+from fasthtml import ft
 from fasthtml.common import fast_app, serve
 
-app, rt = fast_app()
+from app import utils
+
+app, rt = fast_app(on_startup=[utils.pull_llm])
 
 
 @rt("/")
 def home():
-    return "Hello, world!"
+    return ft.Main(
+        ft.Form(
+            ft.Label("Question", ft.Input(name="question")),
+            ft.Button("Submit"),
+            hx_post="/ask",
+        ),
+        cls="container",
+    )
+
+
+@rt("/ask")
+def post(question: str):
+    return (
+        ft.Label("Question", ft.Input(name="question", value=question, disabled=True)),
+        ft.Label(
+            "Answer",
+            ft.Input(
+                name="answer",
+                value=ollama.generate(model="gemma2:2b", prompt=question)["response"],
+                disabled=True,
+            ),
+        ),
+        ft.Button("Reset", hx_get="/", hx_target="body"),
+    )
 
 
 if __name__ == "__main__":
---
+++ app/utils.py
@@ -0,0 +1,19 @@
+import logging
+
+import ollama
+
+
+logger = logging.getLogger("uvicorn.info")
+
+
+def pull_llm():
+    logger.info("Pulling LLM")
+    last_progress = 0
+    for resp in ollama.pull("gemma2:2b", stream=True):
+        if "completed" not in resp or "total" not in resp:
+            continue
+        current_progress = resp["completed"] / resp["total"]
+        if current_progress - last_progress >= 0.05:
+            logger.info(f"Pulling: {current_progress:.0%}")
+            last_progress = current_progress
+    logger.info("Pulling complete")
